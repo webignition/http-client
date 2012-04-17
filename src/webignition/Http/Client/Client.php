b0264e4809a97bb32416c440ee225f58f6d74768
Jon Cram <jon@webignition.net>
2012-04-17 11:17:55 +0100

    Moved redirect handling to its own class

--- src/webignition/Http/Client/Client.php
+++ src/webignition/Http/Client/Client.php
@@ -10,38 +10,10 @@ namespace webignition\Http\Client;
  * @package webignition\Http\Client
  *
  */
-class Client {
+class Client {   
     
-    const DEFAULT_REDIRECT_LIMIT = 10;
+    private $redirectHandler = null;
     
-    /**
-     * Collection of HTTP codes for which redirects might apply
-     * 
-     * @var array
-     */
-    private $redirectableResponseCodes = array(301, 302, 303, 307, 308);
-    
-    
-    /**
-     * Collection of boolean flags denoting whether to follow redirects for given HTTP codes
-     * 
-     * @var array
-     */
-    private $followRedirectFor = array();
-    
-    
-    /**
-     *
-     * @var int
-     */
-    private $redirectLimit = null;
-    
-    
-    /**
-     *
-     * @var int
-     */
-    private $redirectCount = 0;
     
     /**
      *
@@ -51,7 +23,7 @@ class Client {
     public function getResponse(\HttpRequest $request) {
         $response = $request->send();
         
-        while ($this->followRedirectFor($response->getResponseCode()) && $this->redirectCount < $this->redirectLimit()) {
+        while ($this->redirectHandler()->followRedirectFor($response->getResponseCode()) && !$this->redirectHandler()->isLimitReached()) {
             $request->setUrl($response->getHeader('Location'));
             $this->redirectCount++;
             $response = $request->send();
@@ -63,109 +35,14 @@ class Client {
     
     /**
      *
-     * @return int
+     * @return \webignition\Http\Response\RedirectHandler\RedirectHandler
      */
-    private function redirectLimit() {
-        if (is_null($this->redirectLimit)) {
-            return self::DEFAULT_REDIRECT_LIMIT;
+    public function redirectHandler() {
+        if (is_null($this->redirectHandler)) {
+            $this->redirectHandler = new \webignition\Http\Response\RedirectHandler\RedirectHandler();
         }
         
-        return $this->redirectLimit;
-    }
-    
-    
-    /**
-     *
-     * @param int $redirectLimit
-     * @return boolean 
-     */
-    public function setRedirectLimit($redirectLimit) {
-        if (is_string($redirectLimit)) {
-            $redirectLimit = (int)$redirectLimit;
-        }
-        
-        if (!is_int($redirectLimit)) {
-            return false;           
-        }
-        
-        return $this->redirectLimit = $redirectLimit;
-    }
-    
-    
-    /**
-     *
-     * @param int $responseCode
-     * @return boolean
-     */
-    private function isRedirectableResponseCode($responseCode) {        
-        if (is_string($responseCode)) {
-            $responseCode = (int)$responseCode;
-        }
-        
-        if (!is_int($responseCode)) {
-            return false;
-        }
-        
-        return in_array($responseCode, $this->redirectableResponseCodes);
-    }
-    
-    
-    /**
-     *
-     * @param int $forResponseCode
-     */
-    public function enableFollowRedirect($forResponseCode = false) {
-        if ($forResponseCode === false) {
-            foreach ($this->redirectableResponseCodes as $httpStatus) {
-                $this->enableFollowRedirectFor($httpStatus);
-            }
-        } else {
-            $this->enableFollowRedirectFor($forResponseCode);
-        }
-    }
-    
-    
-    /**
-     *
-     * @param int $forResponseCode
-     * @return null 
-     */
-    public function disableFollowRedirect($forResponseCode = false) {
-        if ($forResponseCode === false) {
-            foreach ($this->redirectableResponseCodes as $httpStatus) {
-                $this->disableFollowRedirectFor($httpStatus);
-            }
-        } else {
-            $this->disableFollowRedirectFor($forResponseCode);
-        }
-    }
-    
-    
-    /**
-     *
-     * @param int $responseCode
-     * @return boolean 
-     */
-    private function followRedirectFor($responseCode) {
-        return isset($this->followRedirectFor[$responseCode]) && $this->followRedirectFor[$responseCode] === true;
-    }
-    
-    
-    /**
-     *
-     * @param int $httpStatus 
-     */
-    private function enableFollowRedirectFor($forResponseCode) {
-        $this->followRedirectFor[$forResponseCode] = true;
-    }
-    
-    /**
-     *
-     * @param int $httpStatus 
-     */
-    private function disableFollowRedirectFor($forResponseCode) {
-        $this->followRedirectFor[$forResponseCode] = false;
-
-    }    
+        return $this->redirectHandler;
+    }  
 
 }
---
+++ src/webignition/Http/Response/RedirectHandler/RedirectHandler.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace webignition\Http\Response\RedirectHandler;
+
+/**
+ * Handles 30X response handling; figures out the new request URL, sets limits on redirects to prevent unending redirect loops
+ *
+ * @package webignition\Http\Response\RedirectHandler
+ *
+ */
+class RedirectHandler {
+    
+    const DEFAULT_REDIRECT_LIMIT = 10;
+    
+    /**
+     * Collection of HTTP codes for which redirects might apply
+     * 
+     * @var array
+     */
+    private $redirectableResponseCodes = array(301, 302, 303, 307, 308);
+    
+    
+    /**
+     * Collection of boolean flags denoting whether to follow redirects for given HTTP codes
+     * 
+     * @var array
+     */
+    private $followRedirectFor = array();
+    
+    
+    /**
+     *
+     * @var int
+     */
+    private $limit = null;
+    
+    
+    /**
+     *
+     * @var int
+     */
+    private $count = 0;
+    
+    
+    /**
+     *
+     * @return boolean
+     */
+    public function isLimitReached() {
+        return $this->count >= $this->limit();
+    }
+    
+    
+    /**
+     *
+     * @return int
+     */
+    private function limit() {
+        if (is_null($this->limit)) {
+            return self::DEFAULT_REDIRECT_LIMIT;
+        }
+        
+        return $this->limit;
+    }
+    
+    
+    /**
+     *
+     * @param int $limit
+     * @return boolean 
+     */
+    public function setLimit($limit) {
+        if (is_string($limit)) {
+            $limit = (int)$limit;
+        }
+        
+        if (!is_int($limit)) {
+            return false;           
+        }
+        
+        return $this->redirectLimit = $limit;
+    }
+    
+    
+    /**
+     *
+     * @param int $forResponseCode
+     */
+    public function enable($forResponseCode = false) {
+        if ($forResponseCode === false) {
+            foreach ($this->redirectableResponseCodes as $httpStatus) {
+                $this->enableFollowRedirectFor($httpStatus);
+            }
+        } else {
+            $this->enableFollowRedirectFor($forResponseCode);
+        }
+    }
+    
+    
+    /**
+     *
+     * @param int $forResponseCode
+     * @return null 
+     */
+    public function disable($forResponseCode = false) {
+        if ($forResponseCode === false) {
+            foreach ($this->redirectableResponseCodes as $httpStatus) {
+                $this->disableFollowRedirectFor($httpStatus);
+            }
+        } else {
+            $this->disableFollowRedirectFor($forResponseCode);
+        }
+    }
+    
+    
+    /**
+     *
+     * @param int $responseCode
+     * @return boolean 
+     */
+    public function followRedirectFor($responseCode) {
+        return isset($this->followRedirectFor[$responseCode]) && $this->followRedirectFor[$responseCode] === true;
+    }
+    
+    
+    /**
+     *
+     * @param int $httpStatus 
+     */
+    private function enableFollowRedirectFor($forResponseCode) {
+        $this->followRedirectFor[$forResponseCode] = true;
+    }
+    
+    /**
+     *
+     * @param int $httpStatus 
+     */
+    private function disableFollowRedirectFor($forResponseCode) {
+        $this->followRedirectFor[$forResponseCode] = false;
+
+    }    
+
+}
