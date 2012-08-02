1240a39c1bfbab24d7688cf8f6d9d8759d0bea53
Jon Cram <jon@webignition.net>
2012-08-02 16:14:50 +0100

    Identify most (all?) curl-related errors and raise relevant CurlException

--- src/webignition/Http/Client/Exception.php
+++ src/webignition/Http/Client/Exception.php
@@ -29,6 +29,7 @@ class Exception extends \Exception {
      */
     private $messages = array(
         -1 => 'An unknown exception occured. Examine $this->getPrevious() for details of underlying exception.',
+        6 => 'Couldn\'t resolve host. The given remote host was not resolved.',
         3 => 'URL malformat. The syntax was not correct.',
         28 => 'Operation timeout. The specified time-out period was reached according to the conditions.'
     );
@@ -38,74 +39,14 @@ class Exception extends \Exception {
      *
      * @param \Exception $previousException 
      */
-    public function __construct(\Exception $previousException) {     
+    public function __construct(\Exception $previousException) {        
         parent::__construct(0, null, $previousException);
-        $this->code = $this->deriveCode();
-        $this->message = $this->messages[$this->getCode()];
-    }
-    
-    
-    /**
-     * Dervives error code based on child exceptions
-     * 
-     * @return null 
-     */
-    private function deriveCode() {                     
-        if ($this->isInvalidUrlException()) {
-            return self::CURL_MALFORMED_URL_EXIT_CODE;
-        }  
+        $rootException = $this->getRootException();
+        
+        if (isset($rootException->curlCode) && $rootException->curlCode > 0) {
+            throw new CurlException($rootException);
+        }
 
-        if ($this->isTimeoutException()) {
-            return self::CURL_TIMEOUT_EXIT_CODE;
-        }
-        
-        return -1;
-    }
-    
-    
-    /**
-     *
-     * @return \HttpRequestException|null 
-     */
-    public function getHttpRequestException() {
-        $previous = $this->getPrevious();
-        if (is_null($previous)) {
-            return null;
-        }
-        
-        if ($previous instanceof \HttpRequestException) {
-            return $previous;
-        }
-        
-        $previous = $previous->getPrevious();
-        if ($previous instanceof \HttpRequestException) {
-            return $previous;
-        }
-        
-        return null;
-    }
-    
-    
-    /**
-     *
-     * @return \webignition\Http\Client\HttpEncodingException|null 
-     */
-    public function getHttpEncodingException() {
-        $previous = $this->getPrevious();
-        if (is_null($previous)) {
-            return null;
-        }
-        
-        if ($previous instanceof \HttpEncodingException) {
-            return $previous;
-        }
-        
-        $previous = $previous->getPrevious();
-        if ($previous instanceof \HttpEncodingException) {
-            return $previous;
-        }
-        
-        return null;       
     }
     
     /**
@@ -120,82 +61,5 @@ class Exception extends \Exception {
         }
         
         return $rootException;
-    }    
-    
-    /**
-     *
-     * @return boolean
-     */
-    public function hasHttpRequestException() {
-        return $this->getHttpRequestException() instanceof \HttpRequestException;
-    }
-    
-    
-    /**
-     *
-     * @return boolean
-     */
-    public function hasHttpEncodingException() {
-        return $this->getHttpEncodingException() instanceof \HttpEncodingException;
-    }    
-    
-    
-    /**
-     *
-     * @return boolean 
-     */
-    public function isInvalidUrlException() {
-        if (!$this->hasHttpRequestException()) {
-            return false;
-        }
-        
-        $trace = $this->getHttpRequestException()->getTrace();
-        foreach ($trace as $traceItem) {
-            if (count($traceItem['args']) > 0) {
-                if ($traceItem['args'][0] instanceof \HttpRequest) {
-                    /* @var $httpRequest \HttpRequest */
-                    
-                    $httpRequest = $traceItem['args'][0];
-                    $responseInfo = $httpRequest->getResponseInfo();
-                    $effectiveUrl = $responseInfo['effective_url'];
-                    
-                    if (substr($effectiveUrl, 0, strlen(self::INVALID_URL_PREFIX)) == self::INVALID_URL_PREFIX) {
-                        return true;
-                    }
-                }
-            }             
-        }
-        
-        return false;
-    }
-    
-    
-    /**
-     *
-     * @return boolean 
-     */
-    public function isTimeoutException() {
-        if (!$this->hasHttpRequestException()) {
-            return false;
-        }
-        
-        if ($this->isInvalidUrlException()) {
-            return false;
-        }
-        
-        return $this->getHttpRequestException()->curlCode == self::CURL_TIMEOUT_EXIT_CODE;
-    }
-    
-    
-    /**
-     *
-     * @return boolean 
-     */
-    public function isDnsLookupFailureException() {
-        if (!$this->getRootException() instanceof \HttpRequestException) {
-            return false;
-        }
-        
-        return $this->getRootException()->curlCode == self::CURLE_COULDNT_RESOLVE_HOST;
     }
 }
